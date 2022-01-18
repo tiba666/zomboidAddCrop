654ab6f365c6e2a1af82ecf975a96826aa52c2ab
tiba666 <519338+tiba666@users.noreply.github.com>
2022-01-18 17:05:37 +0100

    fixed the names

---
+++ mods/NewCropExample/media/lua/server/Items/foo_VehicleDistributions.lua
@@ -0,0 +1,6 @@
+require 'Vehicles/VehicleDistributions'
+
+
+
+table.insert(VehicleDistributions["FarmerTruckBed"].items, "foofarming.fooSeedbag");
+table.insert(VehicleDistributions["FarmerTruckBed"].items, 8);
