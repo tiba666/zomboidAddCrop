76aa240b47a5ff4c3df2bf417fdcf184ba4e542f
tiba666 <519338+tiba666@users.noreply.github.com>
2022-02-04 22:41:21 +0100

    version 2 of the helping pack

--- mods/NewCropExample/media/lua/server/Items/foo_VehicleDistributions.lua
+++
@@ -1,6 +0,0 @@
-require 'Vehicles/VehicleDistributions'
-
-
-
-table.insert(VehicleDistributions["FarmerTruckBed"].items, "foofarming.fooSeedbag");
-table.insert(VehicleDistributions["FarmerTruckBed"].items, 8);
